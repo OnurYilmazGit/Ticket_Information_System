9e58c51703653abc50fa49de1174871361bd4b0a
OnurYilmazGit <46044317+OnurYilmazGit@users.noreply.github.com>
2020-12-15 16:16:17 +0300

    Update Database.java
    Added selecting a user from the database.

--- Database/Database.java
+++ Database/Database.java
@@ -46,12 +46,33 @@ public class Database {
                 main(args);
             }
         }        
+            else if( i == 2) {
+            System.out.print("Please enter your UserName: ");
+            String name_of_user = input.nextLine();
+            System.out.print("Please enter a Password ");
+            String user_password = input.nextLine();
+            String sql = "SELECT * FROM user WHERE (user_name = '" +name_of_user+ "') AND (user_pass ='" +user_password+ "')";
+            try {
+                stmt.executeQuery(sql);
+                System.out.println("Successfully login.");
+            } catch (SQLException throwables) {
+                System.out.println("There is no such a user in database. Plese try again");
+            }
+            System.out.println("-> To see main list again please enter 1 or enter random number to exit.");
+            int number = input.nextInt();
+            if (number == 1) {
+                args = new String[0];
+                main(args);
+            }
+        }    
     }
+    
+    
 
         public static void main (String[]args) throws SQLException{
             Database ex = new Database();
             ex.init();
-            System.out.println("1. Add User\n");
+            System.out.println("1. Add User\n2. Login User\n3");
             System.out.print("Please choose any number from list: ");
             Scanner anything = new Scanner(System.in);
             int i = anything.nextInt();
@@ -60,10 +81,10 @@ public class Database {
             if (i >= 1 && i <= 4) {
                 events(i);
             } else
-                System.out.print("Please enter number between 1 and 12");
+                System.out.print("Please enter number between 1 and 4");
             int s = anything.nextInt();
             while ((s >= 1 && s <= 4) == false) {
-                System.out.print("Please enter number between 1 and 12");
+                System.out.print("Please enter number between 1 and 4");
                 int b = anything.nextInt();
                 s = b;
                 if (s >= 1 && s <= 4) {
